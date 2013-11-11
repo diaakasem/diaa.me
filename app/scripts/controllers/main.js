f5703bc3821fb0f34d356e6f845dfe779c75b300
Diaa Kasem <me@diaa.me>
2013-11-11 06:22:22 +0200

    Initial project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('diaa.meApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
