9a2d0057feef623750a39f5e827a88066ea9261f
Diaa Kasem <me@diaa.me>
2022-02-07 08:51:48 +0200

    Adding dat, upwork redirection, sync code and others

---
+++ sync-s3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -
+#===============================================================================
+#
+#          FILE: sync-s3.sh
+#
+#         USAGE: ./sync-s3.sh
+#
+#   DESCRIPTION:
+#
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: YOUR NAME (),
+#  ORGANIZATION:
+#       CREATED: 12/06/2017 18:56
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+
+# buckets="diaa.me www.diaa.me"
+# Now all requests to www.diaa.me are going to diaa.me
+buckets="diaa.me"
+for bucket in $buckets; do
+    aws s3 sync . s3://$bucket --exclude=".git/*" --exclude=".tmp/*"
+done
+
